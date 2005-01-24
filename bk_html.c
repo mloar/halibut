3dd52aec2e2f1963ee095faef70e7920392508a5
simon <simon@cda61777-01e9-0310-a592-d414129be87e>
2005-01-24 13:35:34 +0000

    Shouldn't consider para_Title to be a heading when going through sections looking for local configuration data.
    git-svn-id: svn://svn.tartarus.org/sgt/halibut@5196 cda61777-01e9-0310-a592-d414129be87e

--- bk_html.c
+++ bk_html.c
@@ -834,7 +834,8 @@ void html_backend(paragraph *sourceform, keywordlist *keywords,
 	    for (s = sects.head; s; s = s->next) {
 		if (s->file == f && s->text) {
 		    for (p = s->text;
-			 p && (p == s->text || !is_heading_type(p->type));
+			 p && (p == s->text || p->type == para_Title ||
+			       !is_heading_type(p->type));
 			 p = p->next) {
 			if (p->type == para_Config) {
 			    if (!ustricmp(p->keyword, L"html-local-head")) {
